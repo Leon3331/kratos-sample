5dc061cada12c0667f401aa48a486947cb430813
Leon3331 <704937668@qq.com>
2024-07-11 16:54:35 +0800

    initialization

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,6 +1,23 @@
 package service
 
-import "github.com/google/wire"
+import (
+	v1 "KRATOS-REALWORLD/api/helloworld/v1"
+	"KRATOS-REALWORLD/internal/biz"
+
+	"github.com/google/wire"
+)
 
 // ProviderSet is service providers.
 var ProviderSet = wire.NewSet(NewRealWorldService)
+
+// GreeterService is a greeter service.
+type RealWorldService struct {
+	v1.UnimplementedRealWorldServer
+
+	uc *biz.GreeterUsecase
+}
+
+// NewGreeterService new a greeter service.
+func NewRealWorldService(uc *biz.GreeterUsecase) *RealWorldService {
+	return &RealWorldService{uc: uc}
+}
